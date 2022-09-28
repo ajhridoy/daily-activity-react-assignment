c44059f9f3874b828a50d9f2bd4add8e20a59868
ajhridoy <ajhridoy461@gmail.com>
2022-09-29 04:36:01 +0600

    self information in cart added

--- src/Components/ActivitisCart/ActivitisCart.js
+++ src/Components/ActivitisCart/ActivitisCart.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 const ActivitisCart = () => {
     return (
-        <div className='bg-teal-300'>
-            <div>
-                <img src="" alt="" />
+        <div className='bg-teal-300 px-5 py-8'>
+            <div className='flex flex-row'>
+                <img className='rounded-full w-14 h-14' src="http://www.marrybd.com/application/views/member/member_images/IMG_75282.JPG" alt="" />
+                <div className='ml-3'>
+                    <h3 className='text-lg font-bold'>AJ Hridoy</h3>
+                    <p>Rajshahi, Bangladesh</p>
+                </div>
+            </div>
+            <div className='flex flex-row mt-5 bg-slate-100 p-4 rounded-lg '>
+                <div>
+                    <h3 className='text-lg font-bold'>70 <small>kg</small></h3>
+                    <p>Weight</p>
+                </div>
+                <div className='ml-4'>
+                    <h3 className='text-lg font-bold'>5.8</h3>
+                    <p>Height</p>
+                </div>
+                <div className='ml-4'>
+                    <h3 className='text-lg font-bold'>23 <small>yrs</small></h3>
+                    <p>Age</p>
+                </div>
             </div>
         </div>
     );
